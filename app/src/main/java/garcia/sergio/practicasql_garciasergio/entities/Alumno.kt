f5b1b6f912d2f9ef9b2a8ed890d65af962a46fa5
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-06-27 12:14:47 -0700

    commit inicial
    dudas

---
+++ app/src/main/java/garcia/sergio/practicasql_garciasergio/entities/Alumno.kt
@@ -0,0 +1,9 @@
+package garcia.sergio.practicasql_garciasergio.entities
+
+data class Alumno(val nombre: String,
+                  val apellidoP: String,
+                  val apellidoM: String,
+                  val carrera: String
+
+
+)
